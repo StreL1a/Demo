c7aa091b3a310222310efd0c5d2608c903a42d15
StreL1a <107192828+StreL1a@users.noreply.github.com>
2023-06-11 09:23:22 +0300

    Add files via upload

---
+++ WEB/script.js
@@ -0,0 +1,10 @@
+new Swiper(".past-ithem", {
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
